3bb379aac7f044e9376f5e70aa29270d43b6f77f
Alaa Masoud <alaa.masoud@live.com>
2019-12-16 01:07:44 +0300

    Fix null exception on string reverse check

--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -5,6 +5,6 @@ namespace FreeCellSolver
     public static class StringExtensions
     {
         public static bool IsReverseOf(this string val, string other)
-            => val[0] == other[1] && val[1] == other[0];
+            => val != null && val[0] == other[1] && val[1] == other[0];
     }
 }
