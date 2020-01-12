f3d53d85e288878e2bfee05e61a5e282a850cc50
Alaa Masoud <alaa.masoud@live.com>
2020-01-12 23:07:19 +0300

    todo!

--- src/Solvers/Shared/ParallelHelper.cs
+++ src/Solvers/Shared/ParallelHelper.cs
@@ -34,6 +34,13 @@ namespace FreeCellSolver.Solvers.Shared
                 }
             }
 
+            // TODO:
+            // If nominees count is > cpu core count then we should .Take(cpuCount-1) then take the parent.
+            // We can avoid processing the same boards by making the solvers delay execution of the last
+            // board in the states array (the parent that contains dups) so that they can be immediatelly
+            // skipped because they'll already be in the closed set.
+            // Same applies when nominees count is < cpu core count. We should take all of them and then
+            // append the parent at the end and have solvers delay it's processing.
             var nominees = new List<HashSet<Board>>();
             foreach (var node in tree[depth - 1])
             {
