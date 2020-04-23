b6ecafd25ed49b2b6fddf4802c83c532d7839d5d
Alaa Masoud <alaa.masoud@live.com>
2020-04-23 07:45:49 +0300

    minor

--- src/Game/Extensions/BoardExtensions.cs
+++ src/Game/Extensions/BoardExtensions.cs
@@ -18,6 +18,7 @@ namespace FreeCellSolver.Game.Extensions
                 20, 19, 18, 17, 16, 15, 14, 13, 12, 11,
                 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 
             };
+
             var seed = dealNum;
 
             for (var i = 0; i < crv.Length; i++)
