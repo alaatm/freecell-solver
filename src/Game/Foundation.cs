ede9d456b3d03cd8986ce691f8d56b5e2efb4f55
Alaa Masoud <alaa.masoud@live.com>
2020-05-08 19:55:26 +0300

    Remove unused usings

--- src/Game/Foundation.cs
+++ src/Game/Foundation.cs
@@ -2,7 +2,6 @@ using System.Text;
 using System.Linq;
 using System.Diagnostics;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
 namespace FreeCellSolver.Game
